54d1540484fad0fc7a2fcee288fb6d6a0e577356
rahulenamala <rahulenamala@gmail.com>
2024-01-22 01:33:43 +0530

    latest changes

--- main.py
+++ main.py
@@ -1,72 +1,3 @@
-# import os
-# import streamlit as st
-# import pickle
-# import time
-# from langchain import OpenAI
-# from langchain.chains import RetrievalQAWithSourcesChain
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.document_loaders import UnstructuredURLLoader
-# from langchain.embeddings import OpenAIEmbeddings
-# from langchain.vectorstores import FAISS
-
-# from dotenv import load_dotenv
-# load_dotenv()  # take environment variables from .env (especially openai api key)
-
-# st.title("RahulBot: News Research Tool ")
-# st.sidebar.title("News Article URLs")
-
-# urls = []
-# for i in range(3):
-#     url = st.sidebar.text_input(f"URL {i+1}")
-#     urls.append(url)
-
-# process_url_clicked = st.sidebar.button("Process URLs")
-# file_path = "faiss_store_openai.pkl"
-
-# main_placeholder = st.empty()
-# llm = OpenAI(temperature=0.9, max_tokens=500)
-
-# if process_url_clicked:
-#     # load data
-#     loader = UnstructuredURLLoader(urls=urls)
-#     main_placeholder.text("Data Loading...Started...✅✅✅")
-#     data = loader.load()
-#     # split data
-#     text_splitter = RecursiveCharacterTextSplitter(
-#         separators=['\n\n', '\n', '.', ','],
-#         chunk_size=1000
-#     )
-#     main_placeholder.text("Text Splitter...Started...✅✅✅")
-#     docs = text_splitter.split_documents(data)
-#     # create embeddings and save it to FAISS index
-#     embeddings = OpenAIEmbeddings()
-#     vectorstore_openai = FAISS.from_documents(docs, embeddings)
-#     main_placeholder.text("Embedding Vector Started Building...✅✅✅")
-#     time.sleep(2)
-
-#     # Save the FAISS index to a pickle file
-#     with open(file_path, "wb") as f:
-#         pickle.dump(vectorstore_openai, f)
-
-# query = main_placeholder.text_input("Question: ")
-# if query:
-#     if os.path.exists(file_path):
-#         with open(file_path, "rb") as f:
-#             vectorstore = pickle.load(f)
-#             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-#             result = chain({"question": query}, return_only_outputs=True)
-#             # result will be a dictionary of this format --> {"answer": "", "sources": [] }
-#             st.header("Answer")
-#             st.write(result["answer"])
-
-#             # Display sources, if available
-#             sources = result.get("sources", "")
-#             if sources:
-#                 st.subheader("Sources:")
-#                 sources_list = sources.split("\n")  # Split the sources by newline
-#                 for source in sources_list:
-#                     st.write(source)
-
 import os
 import streamlit as st
 import pickle
@@ -76,14 +7,7 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import LanceDB  
-from langchain_community.document_loaders import TextLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import LanceDB
-
-import lancedb
-
+from langchain.vectorstores import FAISS
 
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env (especially openai api key)
@@ -97,7 +21,7 @@ for i in range(3):
     urls.append(url)
 
 process_url_clicked = st.sidebar.button("Process URLs")
-file_path = "lance_store_openai.pkl"  # Change the file path for Chroma
+file_path = "faiss_store_openai.pkl"
 
 main_placeholder = st.empty()
 llm = OpenAI(temperature=0.9, max_tokens=500)
@@ -114,13 +38,13 @@ if process_url_clicked:
     )
     main_placeholder.text("Text Splitter...Started...✅✅✅")
     docs = text_splitter.split_documents(data)
-    # create embeddings and save it to Chroma index
+    # create embeddings and save it to FAISS index
     embeddings = OpenAIEmbeddings()
-    vectorstore_openai = LanceDB.from_documents(docs, embeddings)  
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
     main_placeholder.text("Embedding Vector Started Building...✅✅✅")
     time.sleep(2)
 
-    # Save the Chroma index to a pickle file
+    # Save the FAISS index to a pickle file
     with open(file_path, "wb") as f:
         pickle.dump(vectorstore_openai, f)
 
@@ -142,5 +66,3 @@ if query:
                 sources_list = sources.split("\n")  # Split the sources by newline
                 for source in sources_list:
                     st.write(source)
-
-
